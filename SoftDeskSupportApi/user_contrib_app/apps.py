c169e47be9c36a5d955b74fe1f6a98c7df7038ac
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-03 00:00:04 +0100

    chore: initial commit with Django project setup

---
+++ SoftDeskSupportApi/user_contrib_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserContribAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "user_contrib_app"
---
+++ SoftDeskSupportApi/user_contrib_app/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    age = models.IntegerField()
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+
