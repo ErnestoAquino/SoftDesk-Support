35a9b5a34da7f5872ccf06d143eefef350556001
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-03 13:23:01 +0100

    feat: register CustomUser with admin and configure admin options

--- SoftDeskSupportApi/user_contrib_app/admin.py
+++ SoftDeskSupportApi/user_contrib_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+# Registering CustomUser for Django admin interface.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    # Columns to be displayed in the user listing.
+    list_display = ('username', 'age', 'can_be_contacted', 'can_data_be_shared', 'created_time')
+
+    # Filters added to the side of the admin page.
+    list_filter = ('can_be_contacted', 'can_data_be_shared')
+
+    # Adding a search bar to search users by their 'username'.
+    search_fields = ('username',)
