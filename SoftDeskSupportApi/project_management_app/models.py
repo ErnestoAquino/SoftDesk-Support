2dd831a55e0824f8c07f3028010d57ed8a4c2f81
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-06 17:12:52 +0100

    feat: define Project model with relationships and fields

--- SoftDeskSupportApi/project_management_app/models.py
+++ SoftDeskSupportApi/project_management_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from user_contrib_app.models import CustomUser
 
-# Create your models here.
+
+class Project(models.Model):
+    TYPE_CHOICES = (
+        ("backend", "Back-end"),
+        ("frontend", "Front-end"),
+        ("ios", "iOS"),
+        ("android", "Android"),
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+    author = models.ForeignKey(CustomUser,
+                               on_delete=models.CASCADE,
+                               related_name="Authored_projects")
+    contributors = models.ManyToManyField(CustomUser,
+                                          through="user_contrib_app.Contributor",
+                                          related_name="projects")
+    created_time = models.DateTimeField(auto_now_add=True)
