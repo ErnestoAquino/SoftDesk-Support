329e7cbf49b4a2cba4f34341a72e228cdff9eb39
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-06 17:14:09 +0100

    feat: create Contributor model to link users and projects

--- SoftDeskSupportApi/user_contrib_app/models.py
+++ SoftDeskSupportApi/user_contrib_app/models.py
@@ -3,9 +3,24 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
+
+    AGE_MINIMUM = 15
+
     age = models.IntegerField()
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
     created_time = models.DateTimeField(auto_now_add=True)
 
     REQUIRED_FIELDS = ['age']
+
+
+class Contributor(models.Model):
+    user = models.ForeignKey(CustomUser,
+                             on_delete=models.CASCADE,
+                             related_name="contributions")
+    project = models.ForeignKey("project_management_app.Project",
+                                on_delete=models.CASCADE,
+                                related_name="projects_contributors")
+
+    class Meta:
+        unique_together = ("user", "project")
