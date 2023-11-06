44e4b1cbb937a33bebced5e2da39215a0394d3de
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-06 17:18:34 +0100

    feat: implement CustomUsersViewset with create method and age check

--- SoftDeskSupportApi/user_contrib_app/views.py
+++ SoftDeskSupportApi/user_contrib_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status
+# from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
-# Create your views here.
+from user_contrib_app.models import CustomUser
+from user_contrib_app.serializers import CustomUserSerializer
+
+
+class CustomUsersViewset(ModelViewSet):
+
+    serializer_class = CustomUserSerializer
+
+    def get_queryset(self):
+        return CustomUser.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        # Validate data with the serializer
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Check the minimum age.
+        if serializer.validated_data['age'] < CustomUser.AGE_MINIMUM:
+            return Response({"age": [f"The minimum age is {CustomUser.AGE_MINIMUM} years."]},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # If everything is okay, save the instance
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status = status.HTTP_201_CREATED, headers = headers)
