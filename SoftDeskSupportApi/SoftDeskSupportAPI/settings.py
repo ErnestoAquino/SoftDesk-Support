63286e3cac5d8ea972d1dbc6f2fc6036904b388c
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-09 14:13:54 +0100

    feat: configure DRF default pagination with LimitOffsetPagination and page size 2

--- SoftDeskSupportApi/SoftDeskSupportAPI/settings.py
+++ SoftDeskSupportApi/SoftDeskSupportAPI/settings.py
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 AUTH_USER_MODEL = "user_contrib_app.CustomUser"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 2
+}
