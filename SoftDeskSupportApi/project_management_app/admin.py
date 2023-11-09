c77448d755b2a8c83f9190dbb8452c7467ad4f49
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-09 13:34:02 +0100

    feat: add ContributorAdmin to admin with list_display

--- SoftDeskSupportApi/project_management_app/admin.py
+++ SoftDeskSupportApi/project_management_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from project_management_app.models import Project
+from project_management_app.models import Issue
+from project_management_app.models import Comment
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    # Columns to be displayed in the project listing.
+    list_display = ('name', 'type', 'author', 'created_time')
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    # Columns to be displayed in the issue listing.
+    list_display = ('title', 'description', 'status', 'priority', 'tag', 'created_time')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # Columns to be displayed in the project listing.
+    list_display = ('id', 'description', 'author', 'issue', 'created_time')
