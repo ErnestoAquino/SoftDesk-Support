baedfdf2d92931a45e264f7734ac27d78d76543d
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-09 13:44:04 +0100

    feat: create serializers.py for project_management_app

---
+++ SoftDeskSupportApi/project_management_app/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from project_management_app.models import Project
+from project_management_app.models import Issue
+from project_management_app.models import Comment
+from user_contrib_app.serializers import CustomUserSerializer
+
+
+class AuthorSerializerMixin(serializers.Serializer):
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, instance):
+        if instance.author.can_data_be_shared:
+            serializer = CustomUserSerializer(instance.author)
+            return serializer.data
+        else:
+            return {}
+
+
+class ProjectSerializer(AuthorSerializerMixin, ModelSerializer):
+
+    contributors = CustomUserSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = ("id", "name", "description", "type", "created_time", "author", "contributors")
+
+
+class IssueSerializer(AuthorSerializerMixin, ModelSerializer):
+
+    # author = serializers.SerializerMethodField()
+    project = ProjectSerializer()
+
+    class Meta:
+        model = Issue
+        fields = ("id", "title", "description", "status", "priority", "tag", "project", "assignee", "author")
+
+    # def get_author(self, instance):
+    #     if instance.author.can_data_be_shared:
+    #         serializer = CustomUserSerializer(instance.author)
+    #         return serializer.data
+    #     else:
+    #         return {}
+
+
+class CommentSerializer(AuthorSerializerMixin, ModelSerializer):
+    # author = serializers.SerializerMethodField()
+    issue = IssueSerializer()
+
+    class Meta:
+        model = Comment
+        fields = ["id", "description", "author", "issue"]
+
+    # def get_author(self, instance):
+    #     if instance.author.can_data_be_shared:
+    #         serializer = CustomUserSerializer(instance.author)
+    #         return serializer.data
+    #     else:
+    #         return {}
