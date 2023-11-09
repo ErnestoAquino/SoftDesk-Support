6e8632ccd5d2d148b6974149839e2d16113ba62f
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-09 13:40:37 +0100

    feat: create ContributorSerializer with nested CustomUserSerializer

--- SoftDeskSupportApi/user_contrib_app/serializers.py
+++ SoftDeskSupportApi/user_contrib_app/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 
 from user_contrib_app.models import CustomUser
+from user_contrib_app.models import Contributor
 
 
 class CustomUserSerializer(ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ["id", "username", "password", "age", "can_be_contacted", "can_data_be_shared"]
+        fields = ["id", "username", "age", "can_be_contacted", "can_data_be_shared"]
+
+
+class ContributorSerializer(ModelSerializer):
+
+    user = CustomUserSerializer()
+
+    class Meta:
+        model = Contributor
+        fields = ["id", "user", "project"]
