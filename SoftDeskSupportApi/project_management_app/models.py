8779d2fe279f9a86135de99645d161c84cd6a085
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-09 13:35:47 +0100

    feat: define Issue and Comment models with fields and relationships

--- SoftDeskSupportApi/project_management_app/models.py
+++ SoftDeskSupportApi/project_management_app/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from user_contrib_app.models import CustomUser
 
@@ -20,3 +22,54 @@ class Project(models.Model):
                                           through="user_contrib_app.Contributor",
                                           related_name="projects")
     created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Issue(models.Model):
+    STATUS_CHOICES = (
+        ("to_do", "To do"),
+        ("in_progress", "In progress"),
+        ("finished", "Finished"),
+    )
+
+    PRIORITY_CHOICES = (
+        ("low", "Low"),
+        ("medium", "Medium"),
+        ("high", "High"),
+    )
+
+    TAG_CHOICES = (
+        ("bug", "Bug"),
+        ("feature", "Feature"),
+        ("task", "Task")
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="to_do")
+    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES, default="low")
+    tag = models.CharField(max_length=50, choices=TAG_CHOICES)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="issues")
+    assignee = models.ForeignKey(CustomUser,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=True,
+                                 related_name="assigned_issues")
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="created_issues")
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="authored_comments")
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE, related_name="issue_comments")
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.created_time}"
