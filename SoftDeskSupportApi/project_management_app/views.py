334881caa9954cb8ff3699199b374e9335cfcb96
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-09 13:37:53 +0100

    feat: add IssueViewSet and CommentViewSet for issues and comments

--- SoftDeskSupportApi/project_management_app/views.py
+++ SoftDeskSupportApi/project_management_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from project_management_app.models import Project
+from project_management_app.models import Issue
+from project_management_app.models import Comment
+from project_management_app.serializers import ProjectSerializer
+from project_management_app.serializers import IssueSerializer
+from project_management_app.serializers import CommentSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.all()
+
+
+class IssueViewSet(ModelViewSet):
+    serializer_class = IssueSerializer
+
+    def get_queryset(self):
+        return Issue.objects.all()
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.all()
