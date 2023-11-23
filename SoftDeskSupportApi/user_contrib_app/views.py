69c591ff3023edfe63c113c76a34ee9b2eda0e11
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-23 11:27:59 +0100

    feat: update get_permissions in CustomUsersViewSet for action-specific access

--- SoftDeskSupportApi/user_contrib_app/views.py
+++ SoftDeskSupportApi/user_contrib_app/views.py
@@ -1,8 +1,11 @@
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 # from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
+from user_contrib_app.permissions import UserProfilePermission
 from user_contrib_app.models import CustomUser
 from user_contrib_app.models import Contributor
 from user_contrib_app.serializers import CustomUserSerializer
@@ -15,6 +18,18 @@ class CustomUsersViewset(ModelViewSet):
 
     def get_queryset(self):
         return CustomUser.objects.all()
+
+    def get_permissions(self):
+        # Check if the current action is 'create'
+        if self.action == 'create':
+            # If it is 'create', set permissions so that any user can access to allow the creation of a new user
+            permission_classes = [AllowAny]
+        else:
+            # If it's not 'create', restrict access to only authenticated users
+            # and those who have the UserProfilePermission
+            permission_classes = [IsAuthenticated, UserProfilePermission]
+        # Returns a list of instances of the permission classes
+        return [permission() for permission in permission_classes]
 
     def create(self, request, *args, **kwargs):
         # Validate data with the serializer
