fbf271414e166c3b73bd765a9588260162a4e51b
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-23 11:24:07 +0100

    feat: customize get_permissions in ProjectViewSet for action-based permissions

--- SoftDeskSupportApi/project_management_app/views.py
+++ SoftDeskSupportApi/project_management_app/views.py
@@ -1,5 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
+from project_management_app.permissions import IsAuthor
+from project_management_app.permissions import IsContributor
 from project_management_app.models import Project
 from project_management_app.models import Issue
 from project_management_app.models import Comment
@@ -9,6 +14,7 @@ from project_management_app.serializers import IssueListSerializer
 from project_management_app.serializers import IssueDetailSerializer
 from project_management_app.serializers import CommentListSerializer
 from project_management_app.serializers import CommentDetailSerializer
+from user_contrib_app.models import Contributor
 
 
 class BaseViewSet(ModelViewSet):
@@ -29,6 +35,18 @@ class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectListSerializer
     detail_serializer_class = ProjectDetailSerializer
 
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated]
+
+        if self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes.append(IsAuthor)
+        if self.action in ['retrieve', 'list']:
+            permission_classes.append(IsContributor)
+        permission_instances = []
+        for permission in permission_classes:
+            permission_instances.append(permission())
+        return permission_instances
+
     def get_queryset(self):
         return Project.objects.all()
 
@@ -38,12 +56,20 @@ class ProjectViewSet(ModelViewSet):
         else:
             return super().get_serializer_class()
 
+    def perform_create(self, serializer):
+        project = serializer.save(author=self.request.user)
+        Contributor.objects.create(user=self.request.user, project=project)
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
 
 class IssueViewSet(ModelViewSet):
     serializer_class = IssueListSerializer
     detail_serializer_class = IssueDetailSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        if self.action == 'list':
+            return Issue.objects.filter(project__contributors = self.request.user)
         return Issue.objects.all()
 
     def get_serializer_class(self):
