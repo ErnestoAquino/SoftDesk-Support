0ad3433b7ba514239d0f37bc097649f2c580da67
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-11-28 10:10:58 +0100

    feat: update fields in ContributorSerializer to SlugRelatedField and HyperlinkedRelatedField

--- SoftDeskSupportApi/user_contrib_app/serializers.py
+++ SoftDeskSupportApi/user_contrib_app/serializers.py
@@ -1,7 +1,12 @@
+from rest_framework.relations import SlugRelatedField
 from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import PrimaryKeyRelatedField
+from rest_framework.serializers import HyperlinkedRelatedField
+from rest_framework.serializers import ValidationError
 
 from user_contrib_app.models import CustomUser
 from user_contrib_app.models import Contributor
+from project_management_app.models import Project
 
 
 class CustomUserSerializer(ModelSerializer):
@@ -13,8 +18,21 @@ class CustomUserSerializer(ModelSerializer):
 
 class ContributorSerializer(ModelSerializer):
 
-    user = CustomUserSerializer()
+    # user = PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
+    user = SlugRelatedField(slug_field="username", queryset=CustomUser.objects.all())
+    # project = PrimaryKeyRelatedField(queryset=Project.objects.all())
+    project = HyperlinkedRelatedField(view_name="projects-detail",
+                                      queryset=Project.objects.all(),
+                                      lookup_field="pk")
 
     class Meta:
         model = Contributor
-        fields = ["id", "user", "project"]
+        fields = ["user", "project"]
+
+    # def validate(self, data):
+    #     # Check if a Contributor with the same user and project already exists
+    #     if Contributor.objects.filter(user=data["user"], project=data["project"]).exists():
+    #         raise ValidationError({
+    #             "non_field_errors": ["This user is already a contributor to this project."]
+    #         })
+    #     return data
