421a035d2af776942d5d9b27b92e01e6d37982ec
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-12-12 15:15:58 +0100

    docs: refine and correct documentation for permission classes

--- SoftDeskSupportApi/user_contrib_app/serializers.py
+++ SoftDeskSupportApi/user_contrib_app/serializers.py
@@ -1,25 +1,16 @@
 from rest_framework.relations import SlugRelatedField
 from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import PrimaryKeyRelatedField
-from rest_framework.serializers import HyperlinkedRelatedField
 from rest_framework.serializers import ValidationError
 from rest_framework.serializers import CharField
 
 from user_contrib_app.models import CustomUser
 from user_contrib_app.models import Contributor
-from project_management_app.models import Project
 
 
 class CustomUserSerializer(ModelSerializer):
     """
-    Serializer for the CustomUser model.
-
-    This serializer handles the creation and updating of users. For creation, it ensures
-    a password is provided, and for updates, it securely manages password updates along
-    with other user fields.
-
-    The password is treated as a write-only field for security and is made optional in updates to allow
-    partial updates without modifying the password.
+    Handles user creation and updates. Passwords are managed securely and required for new users.
     """
     password = CharField(write_only=True, required=False)
 
@@ -29,10 +20,7 @@ class CustomUserSerializer(ModelSerializer):
 
     def create(self, validated_data):
         """
-        Creates a new user with the validated data.
-
-        Ensures that a password is provided when creating a new user.
-        Raises a ValidationError if not provided.
+        Creates a new user, ensuring password provision.
         """
         if 'password' not in validated_data:
             raise ValidationError({"password": "Password is required to create a user."})
@@ -52,10 +40,7 @@ class CustomUserSerializer(ModelSerializer):
 
     def update(self, instance, validated_data):
         """
-        Updates an existing user with the validated data.
-
-        If a password is included in the validated data, updates the user's password.
-        Updates other user fields as necessary.
+        Updates user information, handling password changes securely.
         """
         if 'password' in validated_data:
             password = validated_data.pop('password')
@@ -72,13 +57,7 @@ class CustomUserSerializer(ModelSerializer):
 
 class ContributorSerializer(ModelSerializer):
     """
-    Serializer for the Contributor model.
-
-    This serializer is used for handling the relationships between users and projects.
-    It allows the addition and removal of contributors to a project by managing the user-project association.
-
-    The 'user' field is represented by the username of the CustomUser model, allowing for easy identification
-    and association. The 'project' field is read-only, as the project association is managed through the endpoint URL.
+    Manages contributors to projects, linking users with projects.
     """
 
     user = SlugRelatedField(slug_field="username", queryset=CustomUser.objects.all())
